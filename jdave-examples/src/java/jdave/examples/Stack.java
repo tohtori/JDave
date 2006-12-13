dc8c0d49a2c64e168b936cd433cfa308582b4481
penberg <penberg@79e85c52-7824-0410-bf8d-df7723250c3f>
2006-12-13 09:05:56 +0000

    Make the example compile.

---
+++ jdave-examples/src/java/jdave/examples/Stack.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave.examples;
+
+/**
+ * @author Joni Freeman
+ * @author Pekka Enberg
+ */
+public class Stack<T> {
+    public boolean empty() {
+        return false;
+    }
+
+    public boolean full() {
+        return false;
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public void push(T element) {
+    }
+}
---
+++ jdave-examples/src/test/jdave/examples/StackSpec.java
@@ -0,0 +1,88 @@
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave.examples;
+
+import jdave.Block;
+import jdave.Context;
+import jdave.Specification;
+
+/**
+ * @author Joni Freeman
+ * @author Pekka Enberg
+ */
+public class StackSpec extends Specification<Stack<?>> {
+    @Context
+    public class EmptyStack {
+        private Stack<String> stack;
+
+        public Stack<String> context() {
+            stack = new Stack<String>();
+            return stack;
+        }
+
+        public void shouldBeEmpty() {
+            specify(should.be.empty());
+        }
+
+        public void shouldNoLongerBeEmptyAfterPush() {
+            stack.push("anything");
+            specify(should.not().be.empty());
+        }
+    }
+
+    @Context
+    public class FullStack {
+        private Stack<Integer> stack;
+
+        public Stack<Integer> context() {
+            stack = new Stack<Integer>();
+            for (int i = 0; i < 10; i++) {
+                stack.push(i);
+            }
+            return stack;
+        }
+
+        public void shouldBeFull() {
+            specify(should.be.full());
+        }
+
+        public void shouldComplainOnPush() {
+            specify(new Block() {
+                public void run() throws Exception {
+                    stack.push(100);
+                }
+            }, should.raise(StackOverflowError.class));
+        }
+    }
+
+    @Context
+    public class StackWhichIsNeitherEmptyNorFull {
+        private Stack<Integer> stack;
+
+        public Stack<Integer> context() {
+            stack = new Stack<Integer>();
+            for (int i = 0; i < 10; i++) {
+                stack.push(i);
+            }
+            return stack;
+        }
+
+        public void shouldAddToTheTopWhenSentPush() {
+            stack.push(new Integer(100));
+            specify(stack.peek(), should.equal(new Integer(100)));
+        }
+    }
+}
