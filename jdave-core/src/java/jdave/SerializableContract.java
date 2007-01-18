0b8625184e3d4c9c69feeafde0d18aa2f1c8e3b0
inhuman <inhuman@79e85c52-7824-0410-bf8d-df7723250c3f>
2007-01-18 20:04:40 +0000

    Add Serializable contract enforcement.

---
+++ jdave-core/src/java/jdave/SerializableContract.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+
+/**
+ * @author Joni Freeman
+ */
+public class SerializableContract implements Contract {
+    public void isSatisfied(Object obj) throws ExpectationFailedException {
+        ObjectOutputStream stream = null;
+        try {
+            stream = new ObjectOutputStream(new OutputStream() {
+                @Override
+                public void write(int b) {                    
+                }
+            });
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        try {
+            stream.writeObject(obj);
+        } catch (IOException e) {
+            throw new ExpectationFailedException(obj + " is not serializable");
+        } finally {
+            try {
+                stream.close();
+            } catch (IOException e) {
+            }
+        }
+    }
+}
---
+++ jdave-core/src/test/jdave/SerializableContractTest.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright 2006 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Joni Freeman
+ */
+public class SerializableContractTest extends TestCase {
+    private Specification<Object> spec;
+
+    @Override
+    protected void setUp() throws Exception {
+        spec = new Specification<Object>() {};
+    }
+
+    public void testSerializableIsAccepted() {
+        spec.specify(5, spec.satisfies(new SerializableContract()));
+    }
+    
+    public void testNonSerializableIsNotAccepted() {
+        try {
+            spec.specify(new NonSerializable(), spec.satisfies(new SerializableContract()));
+            fail();
+        } catch (ExpectationFailedException e) {            
+        }
+    }
+    
+    private static class NonSerializable {        
+    }
+}
