d9fae4ae94ec9544c9c04299d514d429390db02e
penberg <penberg@79e85c52-7824-0410-bf8d-df7723250c3f>
2007-03-14 12:38:14 +0000

    Extract exception related specifications to separate test classes.

---
+++ jdave-core/src/test/jdave/SpecificationWithExpectingToThrowTest.java
@@ -0,0 +1,175 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Lasse Koskela
+ * @author Pekka Enberg
+ */
+public class SpecificationWithExpectingToThrowTest extends TestCase {
+    private Specification<EmptyStack> specification;
+
+    @Override
+    protected void setUp() throws Exception {
+        specification = new Specification<EmptyStack>() { };
+        specification.be = new EmptyStack();
+    }
+
+    public void testShouldPassWhenExpectedExceptionRaised() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new IllegalArgumentException();
+            }
+        }, specification.raise(IllegalArgumentException.class));
+    }
+
+    public void testShouldPassWhenExpectedExceptionRaisedWithSpecifiedMessage() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new IllegalArgumentException("argument is null");
+            }
+        }, specification.raise(IllegalArgumentException.class, "argument is null"));
+    }
+
+    public void testShouldPassWhenExpectedExceptionRaisedExpectingNullMessage() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new IllegalArgumentException((String) null);
+            }
+        }, specification.raise(IllegalArgumentException.class, null));
+    }
+
+    public void testShouldPassWhenExpectedThrowableRaised() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new Throwable();
+            }
+        }, specification.raise(Throwable.class));
+    }
+    
+    public void testShouldPassWhenSubclassOfExpectedThrowableRaised() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new NoClassDefFoundError();
+            }
+        }, specification.raise(Throwable.class));
+    }
+    
+    public void testShouldPassWhenExactExpectedThrowableRaised() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                throw new Throwable();
+            }
+        }, specification.raiseExactly(Throwable.class));
+    }
+    
+    public void testShouldFailWhenExpectingAnExactExceptionButNoneIsRaised() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                }
+            }, specification.raiseExactly(IllegalArgumentException.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block should throw java.lang.IllegalArgumentException but nothing was thrown.", e.getMessage());
+        }
+    }
+    
+    public void testShouldFailWhenExpectingAnExactExceptionButSubclassIsRaised() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException();
+                }
+            }, specification.raiseExactly(Throwable.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block should throw java.lang.Throwable but java.lang.IllegalArgumentException was thrown.", e.getMessage());
+        }
+    }
+    
+    public void testShouldFailWhenExpectedExceptionNotRaised() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                }
+            }, specification.raise(IllegalArgumentException.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block should throw java.lang.IllegalArgumentException but nothing was thrown.", e.getMessage());
+        }
+    }
+    
+    public void testShouldFailWhenDifferentExceptionAsExpectedWasRaised() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new NullPointerException();
+                }
+            }, specification.raise(IllegalArgumentException.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block should throw java.lang.IllegalArgumentException but java.lang.NullPointerException was thrown.", e.getMessage());
+        }
+    }
+
+    public void testShouldFailWhenExpectedExceptionRaisedWithWrongMessage() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException("invalid range");
+                }
+            }, specification.raise(IllegalArgumentException.class, "argument is null"));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("Expected the exception message to be \"argument is null\", but was: \"invalid range\".", e.getMessage());
+        }
+    }
+
+    public void testShouldFailWhenExpectedExceptionRaisedButMessageIsNonNullWhenExpectingNull() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException("invalid range");
+                }
+            }, specification.raise(IllegalArgumentException.class, null));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("Expected the exception message to be \"null\", but was: \"invalid range\".", e.getMessage());
+        }
+    }
+
+    public void testShouldFailWhenExpectedExceptionRaisedWithNullMessageWhenExpectingNonNull() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException((String) null);
+                }
+            }, specification.raise(IllegalArgumentException.class, "argument is null"));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("Expected the exception message to be \"argument is null\", but was: \"null\".", e.getMessage());
+        }
+    }
+
+    class EmptyStack {
+        public boolean empty() {
+            return true;
+        }
+    }
+}
---
+++ jdave-core/src/test/jdave/SpecificationWithNotExpectingToThrowTest.java
@@ -0,0 +1,98 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave;
+
+import junit.framework.TestCase;
+
+/**
+ * @author Pekka Enberg
+ */
+public class SpecificationWithNotExpectingToThrowTest extends TestCase {
+    private Specification<EmptyStack> specification;
+
+    @Override
+    protected void setUp() throws Exception {
+        specification = new Specification<EmptyStack>() { };
+        specification.be = new EmptyStack();
+    }
+
+    public void testShouldPassWhenNoExceptionRaised() {
+        specification.specify(new Block() {
+            public void run() throws Throwable {
+                // Intentionally left blank.
+            }
+        }, specification.not().raise(IllegalArgumentException.class));
+    }
+
+    public void testShouldFailIfThrowsExpectedException() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException();
+                }
+            }, specification.not().raise(IllegalArgumentException.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block threw java.lang.IllegalArgumentException", e.getMessage());
+        }
+    }
+
+    public void testShouldFailIfThrowsSubclassOfExpectedException() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException();
+                }
+            }, specification.not().raise(Throwable.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block threw java.lang.IllegalArgumentException", e.getMessage());
+        }
+    }
+
+    public void testShouldFailIfThrowsExactlyExpectedException() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException();
+                }
+            }, specification.not().raiseExactly(IllegalArgumentException.class));
+            fail();
+        } catch (ExpectationFailedException e) {
+            assertEquals("The specified block threw java.lang.IllegalArgumentException", e.getMessage());
+        }
+    }
+
+    public void testShouldRethrowIfThrowsSubclassOfExactlyExpectedException() {
+        try {
+            specification.specify(new Block() {
+                public void run() throws Throwable {
+                    throw new IllegalArgumentException("rethrown");
+                }
+            }, specification.not().raiseExactly(Throwable.class));
+            fail();
+        } catch (RuntimeException e) {
+            assertEquals(IllegalArgumentException.class, e.getCause().getClass());
+            assertEquals("rethrown", e.getCause().getMessage());
+        }
+    }
+
+    class EmptyStack {
+        public boolean empty() {
+            return true;
+        }
+    }
+}
