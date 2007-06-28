21dc6ee9f3858b1813ad7aabaf23a13d984d0546
inhuman <inhuman@79e85c52-7824-0410-bf8d-df7723250c3f>
2007-06-28 17:22:08 +0000

    Add wicket example.

---
+++ jdave-examples/src/java/jdave/examples/swing/wicket/DictionaryPanel.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave.examples.swing.wicket;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.form.Form;
+import org.apache.wicket.markup.html.form.TextField;
+import org.apache.wicket.markup.html.panel.FeedbackPanel;
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.repeater.Item;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.markup.repeater.util.ModelIteratorAdapter;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.model.Model;
+import org.apache.wicket.model.PropertyModel;
+
+/**
+ * @author Joni Freeman
+ */
+public abstract class DictionaryPanel extends Panel {
+    private List<String> hits = new ArrayList<String>();
+    private String query;
+    
+    public DictionaryPanel(String id) {
+        super(id);
+        add(new FeedbackPanel("feedback"));
+        add(new Form("form") {
+            {
+                add(new TextField("query", new PropertyModel(DictionaryPanel.this, "query")));
+            }
+            @Override
+            protected void onSubmit() {
+                hits = getService().lookup(query);
+                if (hits.isEmpty()) {
+                    DictionaryPanel.this.info("No hits.");
+                }
+            }
+        });
+        add(new RefreshingView("words") {
+            @Override
+            protected Iterator<?> getItemModels() {
+                return new ModelIteratorAdapter(hits.iterator()) {
+                    @Override
+                    protected IModel model(Object object) {
+                        return new Model((String) object);
+                    }
+                };
+            }
+
+            @Override
+            protected void populateItem(Item item) {
+                item.add(new Label("word", item.getModelObjectAsString()));
+            }
+        });
+    }
+
+    protected abstract IDictionaryService getService();
+}
---
+++ jdave-examples/src/test/jdave/examples/swing/wicket/DictionarySpec.java
@@ -0,0 +1,84 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave.examples.swing.wicket;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+import jdave.junit4.JDaveRunner;
+import jdave.wicket.ComponentSpecification;
+
+import org.apache.wicket.feedback.FeedbackMessage;
+import org.apache.wicket.markup.repeater.RefreshingView;
+import org.apache.wicket.model.IModel;
+import org.apache.wicket.util.tester.FormTester;
+import org.jmock.Expectations;
+import org.junit.runner.RunWith;
+
+/**
+ * @author Joni Freeman
+ */
+@RunWith(JDaveRunner.class)
+public class DictionarySpec extends ComponentSpecification<DictionaryPanel> {
+    private DictionaryPanel dictionary;
+    private IDictionaryService service = mock(IDictionaryService.class);
+    
+    public class WhenDictionaryContainsGivenWord {
+        public DictionaryPanel create() {
+            checking(new Expectations() {{ 
+                one(service).lookup("hello"); will(returnValue(Arrays.asList("hei", "moi")));
+            }});
+            return dictionary = startComponent();
+        }
+        
+        public void thePanelListsAllWords() {
+            query("hello");
+            RefreshingView words = (RefreshingView) dictionary.get("words");
+            specify(words, containsInOrder("hei", "moi"));
+        }
+    }
+    
+    public class WhenDictionaryDoesNotContainGivenWord {
+        public DictionaryPanel create() {
+            checking(new Expectations() {{ 
+                one(service).lookup("hello"); will(returnValue(Collections.emptyList()));
+            }});
+            return dictionary = startComponent();
+        }
+        
+        public void thePanelDisplaysNoHitsMessage() {
+            query("hello");
+            FeedbackMessage message = dictionary.getFeedbackMessage();
+            specify(message.getMessage(), should.equal("No hits."));
+        }        
+    }
+    
+    private void query(String wordToLookup) {
+        FormTester form = wicket.newFormTester(dictionary.getId() + ":form");
+        form.setValue("query", wordToLookup);
+        form.submit();
+    }
+
+    @Override
+    protected DictionaryPanel newComponent(String id, IModel model) {
+        return new DictionaryPanel(id) {
+            @Override
+            protected IDictionaryService getService() {
+                return service;
+            }            
+        };
+    }
+}
