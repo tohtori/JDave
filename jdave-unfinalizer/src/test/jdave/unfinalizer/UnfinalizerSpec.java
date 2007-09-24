47db42a12896ea2e6e07bfa90905e1a078ec2d5a
tkarkk <tkarkk@79e85c52-7824-0410-bf8d-df7723250c3f>
2007-09-24 13:29:00 +0000

    Setup module.

---
+++ jdave-unfinalizer/src/test/jdave/unfinalizer/UnfinalizerSpec.java
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package jdave.unfinalizer;
+
+import org.junit.runner.RunWith;
+
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+
+/**
+ * @author Tuomas Karkkainen
+ */
+@RunWith(JDaveRunner.class)
+public class UnfinalizerSpec extends Specification<Unfinalizer> {
+    public class Any {
+        public Unfinalizer create() {
+            return new Unfinalizer();
+        }
+
+        public void unfinalizes() {
+            context.removeFinal();
+        }
+    }
+}
