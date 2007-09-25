921084c728bf741588d19a3e0ce189d5cc2ef8d5
tkarkk <tkarkk@79e85c52-7824-0410-bf8d-df7723250c3f>
2007-09-25 13:31:35 +0000

    Removed unneeded script and updated README.

--- jdave-unfinalizer/fix_manifest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd target
-unzip -o jdave-unfinalizer-1.0-SNAPSHOT.jar META-INF/MANIFEST.MF
-sed -i "s/\(Created\)/Premain-Class: jdave.unfinalizer.UnfinalizerInstrumentationLoader\n&/" META-INF/MANIFEST.MF
-zip jdave-unfinalizer-1.0-SNAPSHOT.jar META-INF/MANIFEST.MF
-cd -
