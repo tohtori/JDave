281bcd0c715c2e09140ad476a5ac8b0708a6d3c5
tkarkk <tkarkk@79e85c52-7824-0410-bf8d-df7723250c3f>
2009-02-17 21:33:30 +0000

    settings

--- jdave-guice/src/main/java/jdave/guice/GuiceMock.java
+++ jdave-guice/src/main/java/jdave/guice/GuiceMock.java
@@ -17,6 +17,7 @@ package jdave.guice;
 
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
