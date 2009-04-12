d0641902e5e881f6515bdcdc037a3b1cfdf360f4
amattila <amattila@79e85c52-7824-0410-bf8d-df7723250c3f>
2009-04-12 19:11:48 +0000

    Adding some more error marginal for sleep time spec as it failed on Panda's CI.

--- jdave-wicket-webdriver/src/test/jdave/webdriver/ChannelSpec.java
+++ jdave-wicket-webdriver/src/test/jdave/webdriver/ChannelSpec.java
@@ -65,7 +65,7 @@ public class ChannelSpec extends Specification<Channel> {
             }});
             long start = System.nanoTime();
             context.waitForAjax();
-            specify(executionTimeInMillis(start), is(closeTo(50, 2)));
+            specify(executionTimeInMillis(start), is(closeTo(50, 15)));
         }
 
         private Double executionTimeInMillis(long start) {
